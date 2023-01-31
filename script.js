0c20b1a632d35d5f2f7a7050aa53e04942363906
Verena Sousa <verena.ads19@hotmail.com>
2023-01-31 11:31:44 -0300

    Iniciando a Logica

--- script.js
+++ script.js
@@ -11,17 +11,39 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+
 
 
 // Event-driven
 // programação imperativa
 // callback
 
+function countdown(){
+    setTimeout(function(){
+        let seconds = Number(secondsDisplay.textContent)
+
+        if(seconds <= 0){
+            seconds = 60
+        }
+
+        secondsDisplay.textContent = seconds - 1
+
+        countdown()
+    }, 1000)
+}
+
+
 buttonPlay.addEventListener('click', function(){
     buttonPlay.classList.add('hide')
     buttonPause.classList.remove('hide')
     buttonSet.classList.add('hide')
     buttonStop.classList.remove('hide')
+
+    countdown()
 
 })
 
@@ -47,4 +69,10 @@ buttonSoundOff.addEventListener('click', function(){
 buttonSoundOn.addEventListener('click', function(){
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
+})
+
+
+buttonSet.addEventListener('click', function(){
+    minutes = prompt('Quantos minutos? ')
+    minutesDisplay.textContent = minutes
 })
