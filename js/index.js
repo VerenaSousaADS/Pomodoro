8c7c265d02ec10cf9b7dfae0ea420184d6a5749d
Verena Sousa <verena.ads19@hotmail.com>
2023-02-13 13:10:01 -0300

    Introducao a es6 modules

--- js/index.js
+++ js/index.js
@@ -5,6 +5,10 @@
 // deixar o código mais performático
 // SEM ALTERAR suas funcionalidades
 
+
+import resetControls from "./controls.js"
+import "./timer.js"
+
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
 const buttonStop = document.querySelector('.stop')
@@ -23,46 +27,7 @@ let minutes = Number(minutesDisplay.textContent)
 // programação imperativa
 // callback
 
-function resetControls(){
-    buttonPlay.classList.remove('hide')
-    buttonPause.classList.add('hide')
-    buttonSet.classList.remove('hide')
-    buttonStop.classList.add('hide')
-}
 
-function updateTimerDisplay(minutes, seconds){
-    minutesDisplay.textContent = String(minutes).padStart(2, "0")
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function resetTimer(){
-    updateTimerDisplay(minutes, 0)
-    clearTimeout(timerTimeOut)
-}
-
-function countdown(){
-   timerTimeOut = setTimeout(function(){
-        let seconds = Number(secondsDisplay.textContent)
-        let minutes = Number(minutesDisplay.textContent)
-        
-        updateTimerDisplay(minutes, 0)
-        
-        if(minutes <=0){
-            resetControls()
-            return
-        }
-
-        if(seconds <= 0){
-            seconds = 2
-            --minutes
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1))
-        
-
-        countdown()
-    }, 1000)
-}
 
 
 buttonPlay.addEventListener('click', function(){
